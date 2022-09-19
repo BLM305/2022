463eb1ade21cb4c73947c5e0e741dd7ff7ba5f41
maeyler <ma.eyler@gmail.com>
2022-09-19 17:00:39 +0300

    first week

---
+++ navbar.js
@@ -0,0 +1,43 @@
+"use strict";
+const NAV = 
+`<link href="https://maeyler.github.io/JS/navbar.css" rel="stylesheet">
+<ul>
+    <li class=JavaScript>JavaScript
+      <div class=menu>
+        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction" target=BLM305>MDN Web Docs</a>
+        <a href="https://maeyler.github.io/JS/" target=BLM305>JS Examples</a>
+        <a href="https://javascript.info/" target=BLM305>JS Tutorial</a>
+      </div>
+    </li>
+    <li class=Course>Course
+      <div class=menu>
+        <a href="/2022/Course_outline.html">Outline</a>
+        <a href="https://eloquentjavascript.net/" target=BLM305>Textbook</a>
+        <a href="https://github.com/BLM305/2022" target=BLM305>Repository</a>
+      </div>
+    </li>
+    <li class=Work>Work
+      <div class=menu>
+        <a href="/2022/work/First%20Week">First Week</a>
+      </div>
+    </li>
+</ul>`
+
+function setNavbar() {
+  let nav = document.createElement('nav')
+  nav.innerHTML = NAV
+  nav.id = 'navbar'
+  document.body.append(nav)
+  nav.querySelectorAll("li").forEach(li => {
+    let div = li.firstElementChild
+    li.onmouseenter = () => {
+      div.style.display = 'block'
+      let w = div.offsetWidth - li.offsetWidth
+      let x = li.offsetLeft - w/2
+      let m = nav.offsetWidth - div.offsetWidth
+      div.style.left = Math.min(Math.max(0, x), m)+'px'
+    }
+    li.onmouseleave = () => { div.style.display = '' }
+  })
+}
+setNavbar()
---
+++ work/First Week.md
@@ -0,0 +1,23 @@
+## First Week
+
+* Glance at our [Repository](https://github.com/BLM305/2022)
+* See the [Course outline](../Course_outline.html)
+* Read ["Neden JavaScript?"](https://eyler.blogspot.com/2018/06/neden-javascript.html)
+* See the [JS examples](https://maeyler.github.io/JS/)
+* Study the [First example](Counting.html)
+* Read Chap 1-3 in [Eloquent JavaScript](http://eloquentjavascript.net/)
+* No class work this week
+
+**Attendance and regular class work is required** <br>
+_Derse devam ve düzenli ödev yapmak gerekiyor_
+
+<script src="/2022/navbar.js"></script>
+<style>
+  body { 
+    max-width: 600px; 
+    background: wheat;
+  }
+  #navbar {
+    margin-left: 0;
+  }
+</style>
